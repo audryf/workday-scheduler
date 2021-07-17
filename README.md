9aedbc02183edced7d671e315a941c30650b3e35
Audry Ford <audryford792@gmail.com>
2021-07-16 20:37:41 -0500

    pseudocode JS steps and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler
+## Description
+Created for busy people with a big work load. Plan your day and stay on track with the color coded hour blocks. Start each day with a blank slate!
+## Installation
+No installation needed! Just follow this link to get started.
---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Display current day on page
+    // Parse saved strings back to objects
+        // Load saved values onto page on refresh
+
+// Listen for save button clicks
+    // Get row text field and row id values
+    // Create event objects for entered value at time (use var = []?)
+        // Save the strigified objects in localStorage
+        
+// Get current number of hours (Recheck every 60 seconds)
+    // check each hour to see if it is past
+        // if yes, add color code for past 
+    // check each hour to see if it is present
+        // if yes, add color code for present
+    // check each hour to see if it is future
+        // if yes, add color code for future
+
+// Reset on new day
